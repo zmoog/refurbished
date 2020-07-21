640a01b35556659d254383ba83c1c961f03989e2
Maurizio Branca <m.branca@arduino.cc>
2020-07-21 19:32:29 +0200

    Add setup.py to allow installs as dependency (#15)

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages  # noqa: H301
+
+with open("README.md") as f:
+    long_description = f.read()
+
+NAME = "refurbished"
+REQUIRES = ["lxml >= 4.5.0", "requests >= 2.23.0", "price-parser == 0.3.3"]
+
+setup(
+    name=NAME,
+    version="0.2.0",
+    description="library to search refurbished products on the Apple Store",
+    author_email="maurizio.branca@gmail.com",
+    url="https://github.com/zmoog/refurbished",
+    keywords=[],
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    classifiers=[  # https://pypi.org/classifiers/
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
