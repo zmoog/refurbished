9fc565bb15312943f812e6dd40ac90cd89d46c1b
Yizhou "Andi" Cui <howdy@andicui.xyz>
2020-10-21 19:34:37 +0200

    Small misc. changes and fixes.

--- refurbished/parser.py
+++ refurbished/parser.py
@@ -6,6 +6,7 @@ import decimal
 from dataclasses import dataclass
 
 import bs4
+from urllib.parse import urlparse
 import unicodedata
 from price_parser import Price
 
@@ -21,6 +22,10 @@ class Product:
     price: decimal.Decimal
     previous_price: decimal.Decimal
     savings_price: decimal.Decimal
+    saving_percentage: float = 0
+
+    def __post_init__(self):
+        self.saving_percentage = float(self.savings_price / self.previous_price)
 
 
 def parse_products(page: str):
@@ -30,10 +35,16 @@ def parse_products(page: str):
 
     page = unicodedata.normalize("NFKD", page)
     page = bs4.BeautifulSoup(page, 'html.parser')
+
+    # Getting the domain we're on from the canonical link metadata.
+
+    current_parsed_url = urlparse(page.find("link", rel="canonical").attrs["href"],  scheme='https')
+    store_domain = f'{current_parsed_url.scheme}://{current_parsed_url.netloc}'
+
     products = page.find("div", class_="refurbished-category-grid-no-js").ul.findAll("li")
     return [Product(
         _parse_name(product),
-        _parse_url(product),
+        _parse_url(product, store_domain),
         _parse_current_price(product),
         _parse_previous_price(product),
         _parse_savings_price(product),
@@ -53,22 +64,40 @@ def _parse_current_price(product: bs4.element.Tag) -> decimal.Decimal:
 
 
 def _parse_previous_price(product: bs4.element.Tag) -> decimal.Decimal:
-    """Parse the fragment with the previous product price."""
-    return _extract_price(product.find("span", class_="as-price-previousprice").get_text())
+    """
+    Parse the fragment with the previous product price.
+    If such fragment doesn't exist, assume there is no price reduction.
+    """
+    previousprice_tag = product.find("span", class_="as-price-previousprice")
+    if previousprice_tag:
+        return _extract_price(previousprice_tag.get_text())
+    else:
+        return _parse_current_price(product)
 
 
 def _parse_savings_price(product: bs4.element.Tag) -> decimal.Decimal:
-    """Parse the fragment with the savings price.
-
-    Its value should be equal to `previous` - `current` prices.
     """
-    return _extract_price(product.find("span", class_="as-producttile-savingsprice").get_text())
+    Parse the fragment with the savings price.
+    Its value should be equal to `previous` - `current` prices.
+
+    If such fragment doesn't exist, assume there is no price reduction.
+    """
+    savingsprice_tag = product.find("span", class_="as-producttile-savingsprice")
+    if savingsprice_tag:
+        return _extract_price(savingsprice_tag.get_text())
+    else:
+        return decimal.Decimal(0)
 
 
-def _parse_url(product: bs4.element.Tag) -> str:
-    """Parse the fragment with product URL."""
+def _parse_url(product: bs4.element.Tag, store_domain: str) -> str:
+    """
+    Parse the fragment with product URL.
+    Just appending the relative path onto apple.com does not work for all regions.
+    For example, apple.com/cn redirects to apple.com.cn.
+    All links are then relative to the .cn domain.
+    """
     href = product.h3.a.attrs["href"]
-    return f'https://www.apple.com{href}'
+    return store_domain + href
 
 
 def _extract_price(price_as_text: str) -> decimal.Decimal:
