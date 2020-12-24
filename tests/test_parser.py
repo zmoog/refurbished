115987f91a68ffecfeacacd56b5d3461ff1b917e
Maurizio Branca <m.branca@arduino.cc>
2020-12-24 18:51:24 +0100

    Ignore E501 for test URLs

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -45,7 +45,7 @@ class TestParser:
 
         product = products[0]
         assert product.name == 'iPad Wi-Fi 128GB ricondizionato - Argento (sesta generazione)'
-        assert product.url == 'https://www.apple.com/it/shop/product/FR7K2TY/A/Refurbished-iPad-Wi-Fi-128GB-Silver-6th-Generation?fnode=fdc6d8cf9a3a5596bf835118bf66d1c5629f169110e3c66008a511ee595b8dd45b4d55beb5e42e007c3cc2538e1432961e205bee33610351fa200165b97bfb12c52b24b5670f39ef2da2bb4482e95ada'
+        assert product.url == 'https://www.apple.com/it/shop/product/FR7K2TY/A/Refurbished-iPad-Wi-Fi-128GB-Silver-6th-Generation?fnode=fdc6d8cf9a3a5596bf835118bf66d1c5629f169110e3c66008a511ee595b8dd45b4d55beb5e42e007c3cc2538e1432961e205bee33610351fa200165b97bfb12c52b24b5670f39ef2da2bb4482e95ada'  # noqa: E501
         assert product.price == decimal.Decimal('389.00')
         assert product.previous_price == decimal.Decimal('449.00')
         assert product.savings_price == decimal.Decimal('60.00')
