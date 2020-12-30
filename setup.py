dc88ac95bf66585183f8a21bc6d8c2f07b8debdd
Maurizio Branca <m.branca@arduino.cc>
2020-12-30 12:41:53 +0100

    Publish on PyPI (#37)
    * Rename bin script
    
    * Install bin script from pypi
    
    * Update README with example and contributors
    
    * Small improvement
    
    * Put more emphasis on the CLI usage
    
    * Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -5,14 +5,21 @@ with open("README.md") as f:
     long_description = f.read()
 
 NAME = "refurbished"
-REQUIRES = ["beautifulsoup4 >= 4.9.3", "requests >= 2.25.1", "price-parser == 0.3.4"]
+REQUIRES = [
+    "beautifulsoup4 >= 4.9.3",
+    "requests >= 2.25.1",
+    "price-parser == 0.3.4",
+    "click == 7.1.2"
+]
 
 setup(
     name=NAME,
-    version="0.2.0",
-    description="library to search refurbished products on the Apple Store",
+    version="0.5.0",
+    description="Library to search refurbished products on the Apple Store",
+    author="Maurizio Branca",
     author_email="maurizio.branca@gmail.com",
     url="https://github.com/zmoog/refurbished",
+    scripts=['cli/rfrb'],
     keywords=[],
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
