2d0705f410d864faf681e4b6d0ef9d6f69cbfa9f
Yizhou "Andi" Cui <howdy@andicui.xyz>
2021-02-10 12:22:37 +0100

    Test: Decoding `bytes` to `str` before passing into `parse_products()`

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -19,7 +19,7 @@ class TestParser:
     #         'tests',
     #         'it__ipad__ipad_pro_97__wi_fi.html')
 
-    #     html = io.BytesIO(resource).read()
+    #     html = io.BytesIO(resource).read().decode()
 
     #     products = parse_products(html)
 
@@ -36,16 +36,17 @@ class TestParser:
         """
         resource = pkgutil.get_data(
             'tests',
-            'it_ipad.html')
+            'it_ipad.html'
+        )
 
-        html = io.BytesIO(resource).read()
+        html = io.BytesIO(resource).read().decode()
 
         products = parse_products(html)
         assert len(products) == 34
 
         product = products[0]
         assert product.name == 'iPad Wi-Fi 128GB ricondizionato - Argento (sesta generazione)'
-        assert product.url == 'https://www.apple.com/it/shop/product/FR7K2TY/A/Refurbished-iPad-Wi-Fi-128GB-Silver-6th-Generation?fnode=fdc6d8cf9a3a5596bf835118bf66d1c5629f169110e3c66008a511ee595b8dd45b4d55beb5e42e007c3cc2538e1432961e205bee33610351fa200165b97bfb12c52b24b5670f39ef2da2bb4482e95ada'
+        assert product.url == 'https://www.apple.com/it/shop/product/FR7K2TY/A/Refurbished-iPad-Wi-Fi-128GB-Silver-6th-Generation'
         assert product.price == decimal.Decimal('389.00')
         assert product.previous_price == decimal.Decimal('449.00')
         assert product.savings_price == decimal.Decimal('60.00')
@@ -58,7 +59,7 @@ class TestParser:
             'tests',
             'it_mac.html')
 
-        html = io.BytesIO(resource).read()
+        html = io.BytesIO(resource).read().decode()
 
         products = parse_products(html)
         assert len(products) == 82
