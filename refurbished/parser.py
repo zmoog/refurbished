048aca84a758c934e43b070d502a5c949f703535
Yizhou "Andi" Cui <howdy@andicui.xyz>
2021-02-10 12:21:32 +0100

    Model number implemented.

--- refurbished/parser.py
+++ refurbished/parser.py
@@ -2,11 +2,10 @@
 Parser the Apple Refurbished pages to build useful product data.
 """
 import decimal
-
 from dataclasses import dataclass
-
+import re
 import bs4
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 import unicodedata
 from price_parser import Price
 
@@ -23,9 +22,22 @@ class Product:
     previous_price: decimal.Decimal
     savings_price: decimal.Decimal
     saving_percentage: float = 0
+    model: str = None
 
     def __post_init__(self):
+        """
+        Populate fields that are derivable by other values
+        """
+
         self.saving_percentage = float(self.savings_price / self.previous_price)
+        self.model = re.search("/shop/product/(.[^/]*)/", self.url).group(1)
+
+    def __repr__(self):
+        """
+        A readable version for prints.
+        """
+
+        return f'{self.price} ({"-{:.0%}".format(self.saving_percentage)}) - [{self.model}] {self.name} {self.url}'
 
 
 def parse_products(page: str):
@@ -37,8 +49,7 @@ def parse_products(page: str):
     page = bs4.BeautifulSoup(page, 'html.parser')
 
     # Getting the domain we're on from the canonical link metadata.
-
-    current_parsed_url = urlparse(page.find("link", rel="canonical").attrs["href"],  scheme='https')
+    current_parsed_url = urlparse(page.find("link", rel="canonical").attrs["href"], scheme='https')
     store_domain = f'{current_parsed_url.scheme}://{current_parsed_url.netloc}'
 
     products = page.find("div", class_="refurbished-category-grid-no-js").ul.findAll("li")
@@ -91,13 +102,13 @@ def _parse_savings_price(product: bs4.element.Tag) -> decimal.Decimal:
 
 def _parse_url(product: bs4.element.Tag, store_domain: str) -> str:
     """
-    Parse the fragment with product URL.
+    Parse the fragment with product URL and strip away query strings and fragments.
     Just appending the relative path onto apple.com does not work for all regions.
     For example, apple.com/cn redirects to apple.com.cn.
     All links are then relative to the .cn domain.
     """
     href = product.h3.a.attrs["href"]
-    return store_domain + href
+    return urljoin(store_domain, urlparse(href).path, allow_fragments=False)
 
 
 def _extract_price(price_as_text: str) -> decimal.Decimal:
