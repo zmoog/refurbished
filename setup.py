f27d3ed4e55e524d3dbee2c20152897711613b83
Maurizio Branca <maurizio.branca@gmail.com>
2022-03-05 08:00:26 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ REQUIRES = [
 
 setup(
     name=NAME,
-    version="0.6.0",
+    version="0.7.0",
     description="Library to search refurbished products on the Apple Store",
     author="Maurizio Branca",
     author_email="maurizio.branca@gmail.com",
