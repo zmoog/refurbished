57fce3998c3f85fe01f651224f7c8cbf26226192
Maurizio Branca <maurizio.branca@gmail.com>
2022-10-24 23:57:30 +0200

    Add csv support to the feedback module

--- refurbished/feedback.py
+++ refurbished/feedback.py
@@ -1,4 +1,7 @@
+import csv
+import io
 import json
+from dataclasses import asdict, is_dataclass
 from typing import List
 
 import click
@@ -14,27 +17,55 @@ class Feedback:
     def echo(self, text, nl=True, err=False):
         click.echo(text, nl=nl, err=err)
 
-    def result(self, result):
+    def result(self, result) -> None:
         if self.format == "text":
             click.echo(
                 result.str(),
-                # delefate newlines to the result class
+                # delegate newlines to the result class
                 nl=False,
             )
         elif self.format == "json":
+            # assumption: entries are all pydantic dataclasses
             click.echo(
                 json.dumps(result.data(), indent=2, default=pydantic_encoder),
                 # delegate newline to json.dumps
                 nl=False,
             )
         elif self.format == "ndjson":
-            for product in result.data():
+            # assumption: entries are all pydantic dataclasses
+            for entry in result.data():
                 click.echo(
-                    json.dumps(product, default=pydantic_encoder),
+                    json.dumps(entry, default=pydantic_encoder),
                     # The newline is required by the format to separate the
                     # JSON objects
                     nl=True,
                 )
+        elif self.format == "csv":
+            entries = result.data()
+
+            # we need at least one entry to get the fieldnames from
+            # the pydantic dataclass for the csv header
+            if len(entries) == 0:
+                return
+
+            # assumption: entries are all pydantic dataclasses
+            if not is_dataclass(entries[0]):
+                raise TypeError(
+                    "CSV format requires a list of dataclasses, got "
+                    f"{type(entries[0])}"
+                )
+
+            fieldnames = (
+                entries[0].__pydantic_model__.schema()["properties"].keys()
+            )
+
+            out = io.StringIO()
+            writer = csv.DictWriter(out, fieldnames=fieldnames)
+            writer.writeheader()
+            for entry in entries:
+                writer.writerow(asdict(entry))
+
+            click.echo(out.getvalue(), nl=False)
 
 
 _current_feedback = Feedback("text")
