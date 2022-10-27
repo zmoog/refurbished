a62eb4972ffaaf2e678c00403f418db29081c718
Maurizio Branca <maurizio.branca@gmail.com>
2022-10-28 00:10:01 +0200

    Experiment with rich to format text output

--- refurbished/cli.py
+++ refurbished/cli.py
@@ -1,4 +1,8 @@
+import io
 from typing import List
+
+from rich.console import Console
+from rich.table import Table
 
 from .model import Product
 
@@ -8,17 +12,30 @@ class ProductsResult:
         self.values = values
 
     def str(self) -> str:
-        if len(self.values) == 0:
-            return "No products found"
-        out = ""
+        if not self.values:
+            return "No products found\n"
+
+        table = Table(title="Refurbished Products", width=200)
+
+        table.add_column("Previous Price")
+        table.add_column("Price")
+        table.add_column("Saving Price")
+        table.add_column("Saving Percentage")
+        table.add_column("Name")
+
         for p in self.values:
-            out += (
-                f"{p.previous_price} "
-                f"{p.price} "
-                f"{p.savings_price} "
-                f"({p.saving_percentage * 100}%) {p.name}\n"
+            table.add_row(
+                f"{p.previous_price}",
+                f"{p.price}",
+                f"{p.savings_price}",
+                f"{p.saving_percentage}",
+                p.name,
             )
-        return out
+
+        console = Console(file=io.StringIO())
+        console.print(table)
+
+        return console.file.getvalue()
 
     def data(self) -> List[Product]:
         return self.values
