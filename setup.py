7995fd1abe86bf0863d7da7caa0593aea2789723
Maurizio Branca <maurizio.branca@gmail.com>
2022-10-29 08:15:13 +0200

    Add rich to the required libraries

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ with open("README.md") as f:
 NAME = "refurbished"
 REQUIRES = [
     "beautifulsoup4 >= 4.11.1",
-    "requests >= 2.28.1",
+    "click == 8.1.3",
     "price-parser == 0.3.4",
     "pydantic == 1.10.2",
-    "click == 8.1.3"
+    "requests >= 2.28.1",
+    "rich >= 12.6.0",
 ]
 
 setup(
